b1dcef3759f5757a01b04c625654890f97fba015
guobutao001 <guobutao001@ke.com>
2019-10-29 20:54:16 +0800

    update apiclient

--- src/Http/Apiclient.php
+++ src/Http/Apiclient.php
@@ -18,30 +18,8 @@ class Apiclient
             $this->serviceUrl = $this->rpcConfig[$serviceName]['host'];
             $this->serviceName = $serviceName;
         } else {
-            throw new CoreException("没有配置文件");
+            throw new CoreException('curl|Apiclient: ini is empty');
         }
-    }
-
-    public function callApi($actionName, $arguments)
-    {
-        $content = json_encode($arguments);
-        $options['http'] = [
-            'timeout' => 5,
-            'method' => 'POST',
-            'header' => 'Content-type:applicaion/x-www-form-urlencoode',
-            'content' => $content,
-        ];
-        //创建资源流上下文
-        $context = stream_context_create($options);
-
-        $get = [
-            'service_name' => $this->serviceName,
-            'action_name' => $actionName
-        ];
-
-        $serviceUrl = $this->serviceUrl  . $get['action_name'];
-        $result = file_get_contents($serviceUrl, false, $context);
-        return json_decode($result, true);
     }
 
     public function curlApi($actionName, $params, $type)
@@ -54,50 +32,57 @@ class Apiclient
 
     public function curlApiMulti($connomains,$post = 0)
     {
-        $handler = curl_multi_init();
-        foreach ($connomains as $i => $value) {
-            $conn[$i] = curl_init($this->serviceUrl . '/' . $value['path']);
-            if ($post === 0 && !empty($value['params'])) {
-                $str = '';
-                foreach ($value['params'] as $k => $val) {
-                    $str = $str . $k . '=' . $val . '&';
-                }
-                $str = trim($str, '&');
-                $conn[$i] = curl_init($this->serviceUrl . '/' . $value['path'] . '?' . $str);
-            }else {
+        if (empty($connomains)) {
+            throw new CoreException('curl|curlApiMulti: params is empty');
+        }
+        try {
+            $handler = curl_multi_init();
+            foreach ($connomains as $i => $value) {
                 $conn[$i] = curl_init($this->serviceUrl . '/' . $value['path']);
+                if ($post === 0 && !empty($value['params'])) {
+                    $str = '';
+                    foreach ($value['params'] as $k => $val) {
+                        $str = $str . $k . '=' . $val . '&';
+                    }
+                    $str = trim($str, '&');
+                    $conn[$i] = curl_init($this->serviceUrl . '/' . $value['path'] . '?' . $str);
+                } else {
+                    $conn[$i] = curl_init($this->serviceUrl . '/' . $value['path']);
+                }
+                curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, 1);
+                if ($post === 1) {
+                    curl_setopt($conn[$i], CURLOPT_POST, 1);
+                    curl_setopt($conn[$i], CURLOPT_POSTFIELDS, $value['params']);
+                }
+
+                curl_multi_add_handle($handler, $conn[$i]);
             }
-            curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, 1);
-            if ($post === 1) {
-                curl_setopt($conn[$i], CURLOPT_POST, 1);
-                curl_setopt($conn[$i], CURLOPT_POSTFIELDS, $value['params']);
+
+            $active = null;
+            do {
+                $result = curl_multi_exec($handler, $active);
+            } while ($result == CURLM_CALL_MULTI_PERFORM);
+            while ($active and $result == CURLM_OK) {
+                if (curl_multi_select($handler) != -1) {
+                    do {
+                        $result = curl_multi_exec($handler, $active);
+                    } while ($result == CURLM_CALL_MULTI_PERFORM);
+                }
             }
 
-            curl_multi_add_handle($handler, $conn[$i]);
-        }
+            foreach ($connomains as $i => $url) {
 
-        $active = null;
-        do {
-            $result = curl_multi_exec($handler, $active);
-        } while ($result == CURLM_CALL_MULTI_PERFORM);
-        while ($active and $result == CURLM_OK) {
-            if (curl_multi_select($handler) != -1) {
-                do {
-                    $result = curl_multi_exec($handler, $active);
-                } while ($result == CURLM_CALL_MULTI_PERFORM);
+                //获取curl获取到的内容
+                $res[$i] = curl_multi_getcontent($conn[$i]);
+
+                curl_close($conn[$i]);
+
+                curl_multi_remove_handle($handler, $conn[$i]);
             }
+            curl_multi_close($handler);
+        }catch (\Exception $e) {
+            throw new CoreException('curl|apic_lient_request_error|paramsType:' . $post . '|$params:' . json_encode($connomains) . '|curl_exception:' . $e->getMessage() . '|curl_error:' . curl_error($handler));
         }
-
-        foreach ($connomains as $i => $url) {
-
-            //获取curl获取到的内容
-            $res[$i] = curl_multi_getcontent($conn[$i]);
-
-            curl_close($conn[$i]);
-
-            curl_multi_remove_handle($handler, $conn[$i]);
-        }
-        curl_multi_close($handler);
 
         return !empty($res) ? $res : [];
     }
