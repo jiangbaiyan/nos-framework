507587fe7d7f695855cd3ceea4958ff603dace7c
guobutao001 <guobutao001@ke.com>
2019-10-30 11:37:10 +0800

    update rpc code

--- src/Http/Apiclient.php
+++ src/Http/Apiclient.php
@@ -3,11 +3,15 @@
 namespace Nos\Http;
 
 use Nos\Exception\CoreException;
+use Nos\Comm\Config;
 
 class Apiclient
 {
     private $serviceUrl;
     private $serviceName;
+    private $appId;
+    private $accessToken;
+
     protected  $rpcConfig = '';
 
     public function __construct($serviceName)
@@ -15,6 +19,8 @@ class Apiclient
         $this->rpcConfig = Config::get('rpc.ini');
         if (array_key_exists($serviceName, $this->rpcConfig)) {
             $this->serviceUrl = $this->rpcConfig[$serviceName]['host'];
+            $this->appId = $this->rpcConfig[$serviceName]['appId'];
+            $this->accessToken = $this->rpcConfig[$serviceName]['accessToken'];
             $this->serviceName = $serviceName;
         } else {
             throw new CoreException('curl|Apiclient: ini is empty');
@@ -34,6 +40,9 @@ class Apiclient
      */
     public function curlApi($actionName, $params, $type)
     {
+        $params['appId'] = $this->appId;
+        $params['accessToken'] = $this->accessToken;
+        $params['timestamp'] = time();
         $url = $this->serviceUrl . '/' . $actionName;
         $res = Request::send($type, $url, $params);
         return $res;
@@ -46,16 +55,21 @@ class Apiclient
      * @param int $post
      * 实例：
      *      $connomains = [
-            [
-                'path'   => '/unified/register',
-                'params' => [ 'id' => 1]
-            ]
-            ];
+    [
+    'path'   => '/unified/register',
+    'params' => [ 'id' => 1]
+    ]
+    ];
      * @return array
      * @throws CoreException
      */
     public function curlApiMulti($connomains,$post = 0)
     {
+        foreach ($connomains as $k => $val) {
+            $connomains[$k]['params']['appId'] = $this->appId;
+            $connomains[$k]['params']['accessToken'] = $this->accessToken;
+            $connomains[$k]['params']['timestamp'] = time();
+        }
         if (empty($connomains)) {
             throw new CoreException('curl|curlApiMulti: params is empty');
         }
