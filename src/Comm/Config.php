37930e99828a40eaebb6c4b213b2c5348eafde10
jiangbaiyan <987082332@qq.com>
2019-11-02 09:22:58 +0800

    配置文件获取增加判断逻辑

--- src/Comm/Config.php
+++ src/Comm/Config.php
@@ -9,6 +9,7 @@
 
 namespace Nos\Comm;
 
+use Nos\Exception\CoreException;
 use Yaf\Config\Ini;
 
 class Config
@@ -17,12 +18,17 @@ class Config
      * 获取配置
      * @param string $path 如db.ini
      * @return mixed
+     * @throws CoreException
      */
     public static function get(string $path) {
         // 如果配置池中没有当前路径下的配置，需要重新获取，否则直接拿来用
         $poolData = Pool::get($path);
         if (empty($poolData)) {
-            $config = new Ini(APP_PATH . "/config/{$path}", ini_get('yaf.environ'));
+            $configPath = APP_PATH . "/config/{$path}";
+            if (!is_file($configPath)) {
+                throw new CoreException("配置文件{$path}不存在");
+            }
+            $config = new Ini($configPath, ini_get('yaf.environ'));
             $poolData = Pool::set($path, $config->toArray());
         }
         return $poolData;
