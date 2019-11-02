c7a6b8449f0cf30bb184407c08aa5539c4013bb5
jiangbaiyan <987082332@qq.com>
2019-11-02 21:15:13 +0800

    优化报错提示

--- src/Comm/Config.php
+++ src/Comm/Config.php
@@ -26,7 +26,7 @@ class Config
         if (empty($poolData)) {
             $configPath = APP_PATH . "/config/{$path}";
             if (!is_file($configPath)) {
-                throw new CoreException("配置文件:{$configPath}不存在");
+                throw new CoreException("config|config_file:{$configPath}_not_exist");
             }
             $config = new Ini($configPath, ini_get('yaf.environ'));
             $poolData = Pool::set($path, $config->toArray());
